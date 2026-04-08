36318b0bb41f73395c5e4f8381fce3f089ddb1a7
sebkahr <sebkahr@gmail.com>
2026-04-08 10:10:11 +0200

    Added model directory and added 3 classes to the model directory

---
+++ webApp/Model/Medlem.cs
@@ -0,0 +1,9 @@
+namespace webApp.Model;
+
+public class Medlem
+{
+    public string Id { get; set; }
+    public string Navn { get; set; }
+    public string MobilNummer { get; set; }
+    public string Email { get; set; }
+}
---
+++ webApp/Model/tøjArtikel.cs
@@ -0,0 +1,11 @@
+namespace webApp.Model;
+
+public class tøjArtikel
+{
+    public string Tøj_id { get; set; }
+    public string Type { get; set; }
+    public string Størrelse { get; set; }
+    public string Farve { get; set; }
+    public string Status { get; set; }
+    public string Ejer_id { get; set; }
+}
