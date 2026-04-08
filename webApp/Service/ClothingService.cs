4155c6a6302f6e4eb3fb1bf0937adf80b406ed8e
luwam09habte <luwam1@hotmail.dk>
2026-04-08 10:21:54 +0200

    Tilføjelse af service klasse til upload af tøj artikel

---
+++ webApp/Service/ClothingService.cs
@@ -0,0 +1,27 @@
+using MongoDB.Driver;
+
+namespace webApp.Service;
+
+
+public class ClothingService
+{
+    private readonly IMongoCollection<Clothing> _clothing;
+
+    public ClothingService()
+    {
+        var client = new MongoClient("mongodb+srv://group1user:group1@cluster01.e90p8p6.mongodb.net/");
+        var database = client.GetDatabase("Workshop");
+
+        _clothing = database.GetCollection<Clothing>("Clothing");
+    }
+
+    public async Task CreateClothing(Clothing item)
+    {
+        await _clothing.InsertOneAsync(item);
+    }
+
+    public async Task<List<Clothing>> GetAllClothing()
+    {
+        return await _clothing.Find(_ => true).ToListAsync();
+    }
+}
