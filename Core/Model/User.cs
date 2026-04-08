8edf5f737cb51a1e8ac61a592b1245d73481c209
luwam09habte <luwam1@hotmail.dk>
2026-04-08 13:29:36 +0200

    fjernet name fra model klassen User

--- Core/Model/User.cs
+++ Core/Model/User.cs
@@ -3,7 +3,7 @@ namespace Core.Model;
 public class User
 {
     public string Id { get; set; }
-    public string Name { get; set; }
+    /*public string Name { get; set; }*/
     public string PhoneNumber { get; set; }
     public string Email { get; set; }
     public string PasswordHash { get; set; }
