03291319e010305e8afccbde21511b858c4fe441
luwam09habte <luwam1@hotmail.dk>
2026-04-08 10:12:01 +0200

    Tilføjelse af service klassen til opretelse af Users

---
+++ webApp/Service/UserService.cs
@@ -0,0 +1,26 @@
+using MongoDB.Driver;
+
+namespace webApp.Service;
+
+public class UserService
+{
+    private readonly IMongoCollection<User> _users;
+
+    public UserService()
+    {
+        var client = new MongoClient("mongodb+srv://group1user:group1@cluster01.e90p8p6.mongodb.net/");
+        var database = client.GetDatabase("Workshop");
+
+        _users = database.GetCollection<User>("Users");
+    }
+
+    public async Task CreateUser(User user)
+    {
+        await _users.InsertOneAsync(user);
+    }
+
+    public async Task<User> GetUserByUsername(string username)
+    {
+        return await _users.Find(u => u.Username == username).FirstOrDefaultAsync();
+    }
+}
