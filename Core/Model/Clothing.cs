56a6c8991e7d3521d0d599b0f05a97056d523f83
sebkahr <sebkahr@gmail.com>
2026-04-09 10:32:26 +0200

    Added css to Clothing page

--- Core/Model/Clothing.cs
+++ Core/Model/Clothing.cs
@@ -2,7 +2,7 @@ namespace Core.Model;
 
 public class Clothing
 { 
-    public string Clothe_id { get; set; }
+    public string ObjectId { get; set; }
     public string Type { get; set; }
     public string Size { get; set; }
     public string Color { get; set; }
